53187ad46186fb03d078dfa172912040471966d1
lsodini <154422471+lsodini@users.noreply.github.com>
2023-12-20 15:16:25 +0100

    Add files via upload

---
+++ libft/allft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsodini <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 13:52:06 by lsodini           #+#    #+#             */
+/*   Updated: 2023/12/18 13:53:14 by lsodini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+size_t	ft_strlcpy(const char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	x;
+
+	x = ft_strlen(src);
+	i = 0;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (x);
+}
