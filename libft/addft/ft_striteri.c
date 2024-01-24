5c989f61b55855b7acca5a30d7ada9f32b1801f3
lsodini <154422471+lsodini@users.noreply.github.com>
2024-01-24 13:38:28 +0100

    Update ft_striteri.c

--- libft/addft/ft_striteri.c
+++ libft/addft/ft_striteri.c
@@ -9,5 +9,28 @@
 /*   Updated: 2024/01/15 12:27:54 by lsodini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void  ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+      unsigned int  i;
 
+      i = 0;
+      while (s[i])
+      {
+              f(i, s + i);
+              i++;
+      }
+}
+/*#include <stdio.h>
+
+void my_toupper(unsigned int i, char *c)
+{
+	*c = ft_toupper(*c);
+}
+int main()
+{
+	char s[]= "abcd";
+	ft_striteri(s, &my_toupper);
+	printf("%s\n", s);	
+}*/
